e8055e569a2d7afcfb69de2690db91ec1edc5823
Martin <mrasumof@yahoo.com>
2015-06-23 12:46:03 -0400

    Update strings.py

--- python/strings.py
+++ python/strings.py
@@ -18,7 +18,15 @@ def donuts(count):
     >>> donuts(99)
     'Number of donuts: many'
     """
-    raise NotImplementedError
+    
+    if count >= 10:
+        print "Number of donuts: many"
+    elif count < 0:
+        print "Number of donuts can't be negative!"
+    else:
+        print "Number of donuts: " + count
+        
+    """raise NotImplementedError"""
 
 
 def both_ends(s):
@@ -37,7 +45,16 @@ def both_ends(s):
     >>> both_ends('xyz')
     'xyyz'
     """
-    raise NotImplementedError
+    num_chars = len(s)
+    
+    if num_chars <= 1:
+        print "String should have more than 1 character!"
+    else:    
+        start = s[0:2]
+        end = s[numchars-2:numchars]
+        print start + end
+
+    ### raise NotImplementedError ###
 
 
 def fix_start(s):
@@ -56,8 +73,16 @@ def fix_start(s):
     >>> fix_start('donut')
     'donut'
     """
-    raise NotImplementedError
-
+    
+    numchar = len(s)
+    
+    if numchar <=1:
+        print "String must have more than 1 character!"
+    else:
+        firstchar = s[0]
+        b = s.replace(firstchar,"*")
+        newword = firstchar + b[1:numchar]
+        print newword
 
 def mix_up(a, b):
     """
@@ -74,7 +99,18 @@ def mix_up(a, b):
     >>> mix_up('pezzy', 'firm')
     'fizzy perm'
     """
-    raise NotImplementedError
+    numcharsa = len(a)
+    numcharsb = len(b)
+
+    if numcharsa < 2 or numcharsb < 2:
+        print "Strings should have 2 or more characters!"
+    else:
+        auxiliar = a
+        first = b[0:2] + a[2:numcharsa]
+        secon = a[0:2] + b[2:numcharsb]
+    
+    print first + " " + secon
+
 
 
 def verbing(s):
@@ -91,7 +127,19 @@ def verbing(s):
     >>> verbing('do')
     'do'
     """
-    raise NotImplementedError
+    
+    numchars = len(s)
+    
+    ending = s[numchars-3:numchars]
+    
+    if numchars < 3:
+        print "String must have at least 3 characters!"
+    elif ending == "ing":
+        s = s + "ly"
+    else:
+        s = s + "ing"
+        
+    return s    
 
 
 def not_bad(s):
@@ -111,7 +159,16 @@ def not_bad(s):
     >>> not_bad("It's bad yet not")
     "It's bad yet not"
     """
-    raise NotImplementedError
+    
+    startnot = s.find("not")
+    startbad = s.find("bad")
+
+    
+    if startnot != -1 and startbad != -1 and startnot < startbad:
+        auxiliar = s[0:startnot] + "good"
+        s = auxiliar
+            
+    return s
 
 
 def front_back(a, b):
@@ -130,4 +187,36 @@ def front_back(a, b):
     >>> front_back('Kitten', 'Donut')
     'KitDontenut'
     """
-    raise NotImplementedError
+    
+    alen = len(a)
+    blen = len(b)
+    
+    if alen == 0 or blen == 0:
+        print "Both strings should have more than 0 characters"
+    else:    
+        if alen%2 == 0:
+           aendfirsthalf = (alen / 2)
+           afirsthalf = a[0:aendfirsthalf]
+        else:
+            aendfirsthalf = (alen / 2)
+            afirsthalf = a[0:aendfirsthalf+1]
+            
+        if blen%2 == 0:
+            bendfirsthalf = (blen / 2)
+            bfirsthalf = b[0:bendfirsthalf]
+        else:
+            bendfirsthalf = (blen / 2)
+            bfirsthalf = b[0:bendfirsthalf+1]
+
+
+        aseconhalf = a[aendfirsthalf:alen]
+        bseconhalf = b[bendfirsthalf:blen]
+
+         
+        newstring = afirsthalf + bfirsthalf + aseconhalf + bseconhalf
+        
+    return newstring
+
+
+    
+    ### raise NotImplementedError ###
